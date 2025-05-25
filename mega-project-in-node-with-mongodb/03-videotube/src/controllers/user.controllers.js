63c1323a4ee8f738366d214eae1fedc683170b2a
sumitjangir179 <sumitjangir179@gmail.com>
2025-05-25 17:36:56 +0530

    feat: verfiy middleware and logout user controller

--- mega-project-in-node-with-mongodb/03-videotube/src/controllers/user.controllers.js
+++ mega-project-in-node-with-mongodb/03-videotube/src/controllers/user.controllers.js
@@ -179,8 +179,23 @@ const refreshAccesToken = asyncHandler(async (req, res) => {
 
 })
 
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(req.user._id, { $set: { refreshToken: undefined } }, { new: true })
+
+    const options = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+    }
+
+    return res.status(200)
+        .clearCookie('accessToken', options)
+        .clearCookie('refreshToken', options)
+        .json(new ApiResponse(200, {}, 'User logged out successfully'))
+})
 
 
 
 
-export { registerUser, loginUser, refreshAccesToken }
+
+
+export { registerUser, loginUser, refreshAccesToken, logoutUser }
